cf395de78c21a756dbfdfbb48d2df1d5bd861a0c
Abhishek Saraswat <77572876+Abhisheksaraswat21@users.noreply.github.com>
2022-02-19 13:09:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,7 @@ audiogo = new Audio('gameover.mp3');
 document.onkeydown = (e) => {
     console.log("key clicked : ", e.keyCode)
 
-// if(e.keyCode == 38)
-// {
-//     //queryselector gives the first element with the class dino
-
-//    let dino = document.querySelector('.dino');
-//     dino.classList.add('animateDino');
-
-//     //700 seconds ke baad remove hojaaye ye class 
-//     setTimeout(()=> {
-//         dino.classList.remove('animateDino');
-//     },700);
-
-// }
+/
 
 audio.play();
 
@@ -32,7 +20,7 @@ if (e.keyCode == 38) {
     dino = document.querySelector('.dino');
     dino.classList.add('animateDino');
 
-    //it meanss that after 700 milliseconds this action will be performed --
+    //it means that after 700 milliseconds this action will be performed --
 
     setTimeout(() => {
         dino.classList.remove('animateDino')
@@ -45,7 +33,6 @@ if (e.keyCode == 39) {
   //  dino.classList.add('animateDino');
 
     dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    //dino class me left ki uski position thi usko thoda sa shift kia haii
     dino.style.left = dinoX + 122 + 'px';
 
 
@@ -59,7 +46,7 @@ if (e.keyCode == 37) {
     dino.classList.add('animateDino');
 
     dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    //dino class me left ki uski position thi usko thoda sa minus kia haii
+    
     dino.style.left = (dinoX - 122) + 'px';
 
 
@@ -74,8 +61,7 @@ setInterval(() => {
     gameOver = document.querySelector(".gameOver");
     obstacle = document.querySelector(".obstacle");
 
-    //ye dino ki left and top ki values hai --
-    // this gives values in pixel so we convert it in int
+    
     dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
     dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
 //ye obstacle ke left our top ki value hai
@@ -88,7 +74,7 @@ setInterval(() => {
     if(offsetX<113 && offsetY<52) {
 
        
-        //game over dikhega class visible hojaygei
+      
     gameOver.innerText= 'Game Over';
 
     obstacle.classList.remove("obstacleAni");
@@ -105,20 +91,19 @@ setInterval(() => {
 
     }
 
-    //cross true ho aur offset mtlb ke ostacle paas ho tab score bhadega---
+    
 
     else if (offsetX< 145 && cross) {
         score +=1;
         updateScore(score);
         cross = false ;
 
-        //ek second ke baad mai firse cross ko false krdunga
+        
         setTimeout(() => {
             cross = true;
         }, 1000);
 
-            // jese jese score bhadega obstacle ki speed bhi bhadegi --
-            //to is varible me animation ki duration li hai maine...usko kam krne se obstacle fast ho jayega
+           
 
             setTimeout(() => {
 
